18bbfc93fc90a7869a8858c3a4670fd4bd1f6930
Lucas Gouveia Belon <lucasgouveiabelon@gmail.com>
2023-02-13 22:43:59 -0300

    I'm at STDIO
    A cada função que avanço me sinto mais e mais confortável com a
    linguagem C. Ler a bendita da documentação realmente faz bem pra
    conhecer uma linguagem. Em breve saberei mexer com boa profundidade de
    entendimento. Ainda preciso reescrever meus projetos feitos em python
    para o C, mas uma coisa de cada vez e vamos avançando nos aprendizados.

---
+++ learnlibs/12stdio/anotacoes.c
@@ -0,0 +1,16 @@
+/* */
+
+# include <stdio.h>
+
+/*
+int main(void){
+    char * name = "Nome um";
+    printf("%s\n",name);
+# tmpnam é depreciado. não funciona.
+    tmpnam(name);
+    printf("%s\n",name);
+
+
+    return 0;
+}
+*/
