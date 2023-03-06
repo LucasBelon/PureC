d69535e0340dcee1e624c6e134dd5abde999d381
Lucas Gouveia Belon <lucasgouveiabelon@gmail.com>
2023-03-06 02:29:37 -0300

    Agora consigo seguir estudando e escrevendo
    Adicionei no learnlibs o que falta escrever. Desse jeito eu consigo
    seguir adicionando itens sem me perder tanto. Vai ficar mais fácil de
    ter uma perspectiva.

--- learnlibs/12stdio/anotacoes.c
+++ learnlibs/12stdio/anotacoes.c
@@ -77,8 +77,65 @@ int main(void){
      * transferir o controle de buffer
      * para uma variável.*/
 
-    /* continuar em fprintf*/
+    /*
+    //fprintf(FILE *, const char *, ...)
+    FILE * teste = fopen("teste","w");
+    fputs("Sou o mestre codador\n", teste);
+    fprintf(teste, "Sou o mestre codador\n");
+    fclose(teste);
+    */
 
-    
+    /*
+//fscanf(FILE *restrict,const char *restrict,...)
+    FILE * teste = fopen("teste","r");
+    char eba[BUFSIZ];
+    fscanf(teste,
+    "%25[a-z A-Z\n a-z A-Z\n]",
+    eba);
+    printf("%s\nNo EBA", eba);
+    printf("BUFSIZ=%d\n ",BUFSIZ);
     return 0;
+    */
+    
+    //printf(const char *fmt, ...)
+    //Eu não preciso anotar sobre esse.
+    //scanf(const char *fmt, ...)
+    //Esse também é velho conhecido.
+
+    /* Existe uma forma de enviar pra arrays
+     * enquanto nos utilizamos do printf.
+    //sprintf(char *, const char *, ...)
+    Copiamos pra char *, formatando const char *
+    usando os dados de ...
+     * 
+     * E é óbvio que tem um equivalente pra scanf
+//sscanf(const char *restrict, const char *restrict, ...)
+    */
+
+    /* Temos como usar uma versão de fprintf
+     * que aceita va_args. Isso pode ser útil
+     * para compor funções complexas de escrita
+     * em arquivo.
+    //vfprintf(FILE *, 
+    //const char *, 
+    //__builtin_va_list)
+     * Tem um equivalente pra vfscanf
+    //vfscanf(FILE *restrict, 
+    //const char *restrict, 
+    //__builtin_va_list)
+    * e o mesmo vale pra vprintf e vscanf.*/
+
+    // Funções para IO de caracteres:
+    // fgetc, fgets, fputc, fputs, getc, gets,
+    // putc, puts, putchar, getchar, ungetc
+
+    // Funções para IO direto:
+    // fread, fwrite
+
+    // Funções de posicionamento em arquivo.
+    // fgetpos, fseek, fsetpos, ftell, rewind
+    
+    // Manuseio de erros:
+    // clearerr, feof, perror
+
 }
