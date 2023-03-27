a4bc59112409eeb168cf29b58e752427def7f170
Lucas Gouveia Belon <lucasgouveiabelon@gmail.com>
2023-03-27 12:37:29 -0300

    Reaching memory management functions

--- learnlibs/13stdlib/anotacoes.c
+++ learnlibs/13stdlib/anotacoes.c
@@ -44,19 +44,46 @@ int main(void){
      * */
 
 
-
-    /* double strtod(const char *, char **) 
+    /* double strtod(const char *nptr,
+     * char **endptr) 
+     * 
+     * Converte a porção inicial da string nptr 
+     * em double. Seu funcionamento se dá 
+     * decompondo o alvo em 3, uma porção inicial
+     * possivelmente com caracteres isspace, uma
+     * porção semelhante à um float, e uma 
+     * sequência de caracteres não reconhecidos.
+     * O formato esperado para conversão ótima é
+     * um sinal, uma sequência não vazia de
+     * caracteres que eventualmente contenham um
+     * ponto (flutuante), então, opcionalmente,
+     * uma sequência indicativa de potência.
+     *
+     * Retorna o valor convertido. Se a conversão
+     * falhar, zero é retornado. Para todos os 
+     * efeitos, a string é armazebada em endptr.
      *
      *
      * */
 
 
     /* long int strtol(
-     * const char *,
-     * char **,
-     * int) 
+     * const char *nptr,
+     * char **endptr,
+     * int base) 
      *
-     *
+     * Converte a porção inicial para long int.
+     * Seu funcionamento se dá 
+     * decompondo o alvo em 3, uma porção inicial
+     * possivelmente com caracteres isspace, uma
+     * porção representando um inteiro por meio 
+     * de um radix numa determinada base, e uma 
+     * sequência de caracteres não reconhecidos.
+     * Esse negócio da base me confundiu. Ver 
+     * exemplos mais tarde
+     * Retorna o valor convertido. Se der out of
+     * range, ele dá erro, igual outras funções 
+     * acima
      * */
 
 
@@ -65,7 +92,7 @@ int main(void){
      * char **,
      * int) 
      *
-     *
+     * Mesma coisa que strtol, mas unsigned.
      * */
 
 
@@ -75,17 +102,23 @@ int main(void){
 
     /* int rand(void) 
      *
-     *
+     * Computa uma sequência de números 
+     * pseudo-aleatórios, de 0 a RAND_MAX.
+     * Retorma um inteiro pseudo-randômico.
      * */
 
 
     /* void srand(unsigned int seed)
      *
+     * Usa seu argumento para calcular uma nova
+     * sequência de números pseudo-aleatórios
+     * para chamadas subsequentes a rand().
+     *
      *
      * */
 
 
-    // Funções de gerenciamento de memória
+    // Funções de gerenciamento de memória pg356
 
     // Funções de comunicação com o ambiente
     // hospedeiro
