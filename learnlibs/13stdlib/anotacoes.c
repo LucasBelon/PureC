19e4b5183514e80e0ae87bc2b37d29106243f569
Lucas Gouveia Belon <lucasgouveiabelon@gmail.com>
2023-03-28 08:55:08 -0300

    Now in stdlib

--- learnlibs/13stdlib/anotacoes.c
+++ learnlibs/13stdlib/anotacoes.c
@@ -113,15 +113,95 @@ int main(void){
      * Usa seu argumento para calcular uma nova
      * sequência de números pseudo-aleatórios
      * para chamadas subsequentes a rand().
-     *
-     *
+     * Não retorna.
      * */
 
 
     // Funções de gerenciamento de memória pg356
 
+    /* void *calloc(size_t nmemb, size_t size)
+     *
+     * Aloca espaço para uma array com nmemb 
+     * membros em que cada termo possui size de 
+     * tamanho.  O espaço é inicializado com 
+     * todos os bytes zero.
+     * Retorna um ponteiro nulo ou um ponteiro 
+     * para o espaço alocado.
+     * */
+
+
+    /* void free(void *ptr)
+     *
+     * Desaloca o espaço apontado por ptr. Se ptr
+     * é nulo, nada acontece. Se o espaço não
+     * tiver sido alocado por malloc, calloc, ou
+     * realloc, ou se já tiver sido liberado por 
+     * free ou realloc, o comportamento é 
+     * indefinido.
+     * Não retorna.
+     * */
+
+
+    /* void *malloc(size_t size)
+     *
+     * Aloca espaço para uma array com nmemb 
+     * membros em que cada termo possui size de 
+     * tamanho.  O espaço é inicializado sem 
+     * alterações prévias na memória.
+     * Retorna um ponteiro nulo ou um ponteiro 
+     * para o espaço alocado.
+     * */
+
+
+    /* void *realloc(void *ptr, size_t size)
+     *
+     * Altera o tamanho do objeto apontado por
+     * ptr para o tamanho especificado por size.
+     * Comportamento semelhante a malloc, não
+     * alterando o conteúdo da memória
+     * previamente existente. Se o espaço não
+     * tiver sido alocado por malloc, calloc, ou
+     * realloc, ou se já tiver sido liberado por 
+     * free ou realloc, o comportamento é 
+     * indefinido.
+     * Retorna ponteiro nulo ou um ponteiro para
+     * o espaço potencialmente movido. 
+     * */
+ 
+
     // Funções de comunicação com o ambiente
-    // hospedeiro
+    // hospedeiro pg 357
+
+    /* void abort(void)
+     *
+     * A função causa a finalização anormal do
+     * programa, a não ser que, recebido o sinal
+     * SIGABRT, não haja retorno do programa.
+     * O comportamento de se descarregar o buffer
+     * fechar streams de arquivos, e apagar 
+     * arquivos temporários é dependente da
+     * implementação. É retornado para o sistema
+     * um sinal para Encerramento MalvSucedido, 
+     * por meio da chamada a raise(SIGABRT)
+     * Essa função não tem a chance de retornar
+     * para quem a chama.
+     * */
+
+
+    /* int atexit( void (*func)(void) )
+     *
+     * Registra a função argumento para ser
+     * chamada no encerramento normal do programa
+     * Retorna 0 se bem sucedido
+     * */
+
+
+    /* void exit(int status)
+     *
+     * causa a finalização normal do programa. Se
+     * mais de uma chamada a exit é registrada
+     * CONTINUAR AQUI --------------------------
+     * */
 
 
     // Funções de busca e ordenação
