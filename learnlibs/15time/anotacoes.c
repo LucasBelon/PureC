1bb0e4bd8a1b475241fa97f2ca381a23d5cfaa48
Lucas Gouveia Belon <lucasgouveiabelon@gmail.com>
2023-03-30 02:23:36 -0300

    Starting to mess with time.
    I'm the fastest programmer alive

---
+++ learnlibs/15time/anotacoes.c
@@ -0,0 +1,21 @@
+/* */
+
+# include <stdio.h>
+# include <stdlib.h>
+
+int main(void){
+    // Componentes do tempo pg 434
+
+
+    // Funções de manipulação do tempo
+
+
+    // Funções de conversão de tempo
+
+
+
+    return 0;
+}
+
+
+
