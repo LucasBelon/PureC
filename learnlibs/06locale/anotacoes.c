06f5ba2979456d1314059652f6f7f9bc039b6c70
Lucas Gouveia Belon <lucasgouveiabelon@gmail.com>
2023-04-07 18:15:18 -0300

    I jumped the locale lib entirely
    and now it's coming back to haunt me

--- learnlibs/06locale/anotacoes.c
+++ learnlibs/06locale/anotacoes.c
@@ -1,13 +1,29 @@
 /* Arquivo para verificacao da lib locale.h*/
 
 # include <stdio.h>
-# include <locale.h>
+
+struct lconv {
+    char *decimal_point;        /* "." */
+    char *thousands_sep;        /* "" */
+    char *grouping;             /* "" */
+    char *int_curr_symbol;      /* "" */
+    char *currency_symbol;      /* "" */
+    char *mon_decimal_point;    /* "" */
+    char *mon_decimal_sep;      /* "" */
+    char *mon_grouping;         /* "" */
+    char *positive_sign;        /* "" */
+    char *negative_sign;        /* "" */
+    char int_frac_digits;   /* CHAR_MAX */
+    char frac_digits;       /* CHAR_MAX */
+    char p_cs_precedes;     /* CHAR_MAX */
+    char p_sep_by_space;    /* CHAR_MAX */
+    char n_cs_precedes;     /* CHAR_MAX */
+    char n_sep_by_space;    /* CHAR_MAX */
+    char p_sign_posn;       /* CHAR_MAX */
+    char n_sign_posn;       /* CHAR_MAX */
+} CONF_LOCALE;
 
 int main(void){
-    struct lconv ESTRUTURA;
-    /* Fazendo ESTRUTURA. e andando pelas
-     * opcoes, vemos quais valores são
-     * armazenados pela estrutura.*/
 
     return 0;
 }
