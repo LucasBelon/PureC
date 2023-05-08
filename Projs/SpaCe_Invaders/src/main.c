f8458e84d181ce8afe5c26c9c0ffb6d74fffd512
Lucas Gouveia Belon <lucasgouveiabelon@gmail.com>
2023-05-08 01:18:39 -0300

    Quase lá.
    Estou tentando terminar o jogo. O problema é que estou apenas no
    celular, o que dificulta seguir os padrões adotados no projeto. A partir
    de agora as coisas ficarão mais fáceis em relação à dificuldade de criar
    novas funções, mas ainda tem chão pela frente.

--- Projs/SpaCe_Invaders/src/main.c
+++ Projs/SpaCe_Invaders/src/main.c
@@ -25,40 +25,6 @@ int main(){
 }
 
 
-// Passo 3: segunda função para emitir lasers. Nesse caso para emitir
-// novos lasers pelas naves.
-
-
-//def emiteLasersNaves(matriz):
-    /* (matriz) -> [bool, int]
-
-          Recebe a matriz do jogo e emite lasers pelas naves (caracter definido
-          em LASER_NAVE) uma posição abaixo da posição da nave que emitiu o
-          laser. Ao emitir o laser já tem que observar: se atingiu o canhão do
-          jogador (caso no qual tem que imprimir um EXPLOSAO no lugar) e se
-          atingiu algum laser do jogador. Em todos esses casos, o laser
-          recém-emitido já tem que sumir da matriz (ele nem chega a ser
-          impresso nesse caso). No primeiro caso tem que informar que o canhão
-          do jogador foi atingido e no segundo caso tem que atualizar a
-          quantidade de lasers atingidos pois a função retorna esses dois
-          valores numa lista.
-
-          Para definir se uma nave deve ou não emitir laser, sorteie um
-          número aleatório entre 0 e 1 (use a função random.randint para isso),
-          inclusive. Se o resultado for 0, não emita o laser para aquela nave.
-          Se o resultado for 1, emita. Essa verificação só deve ser feita para
-          aquelas naves que não possuem nenhuma outra imediatamente abaixo e
-          sempre na ordem da esquerda para a direita da matriz.
-
-          Retorna:
-
-          [True se canhão do jogador foi atingido (False se não), quantidade de
-          lasers atingidos]
-
-          Obs.1: mesmo que o primeiro laser emitido atinja o canhão, tem que
-          varrer a matriz **inteira** para atualizar a quantidade de lasers
-          atingidos antes de retornar*/ 
-
 // Passo 4: primeira função para mover lasers. Nesse caso, para mover
 // os lasers do jogador.
 
